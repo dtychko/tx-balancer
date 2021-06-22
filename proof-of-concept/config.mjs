4d068d2e611e979f4d3f78607db5d411a3d36c29
Dmitry Tychko <dmitry.tychko@targetprocess.com>
2021-06-22 14:24:23 +0300

    Remove proof-of-concept scripts

--- proof-of-concept/config.mjs
+++
@@ -1,6 +0,0 @@
-export const amqpUri = 'amqp://guest:guest@localhost:5672/';
-export const inputQueueName = '_tx_balancer_input';
-export const responseQueueName = '_tx_balancer_response';
-
-export const outputQueueName = index => `_tx_balancer_output_${index}`;
-export const outputMirrorQueueName = index => `${outputQueueName(index)}.mirror`;
