fbbfc20cd8cafe3e645c222db03b32f98ef7a93c
Dmitry Tychko <dmitry.tychko@targetprocess.com>
2021-06-25 17:06:55 +0300

    Add unit tests for LinkedListQueue

--- src/balancing/LinkedListQueue.spec.ts
+++ src/balancing/LinkedListQueue.spec.ts
@@ -1,17 +1,60 @@
 import {LinkedListQueue} from './LinkedListQueue'
 
-test('should dequeue last node with predicate', () => {
+test('enqueue then dequeue', () => {
+  const queue = new LinkedListQueue<number>()
+  for (let i = 0; i < 10; i++) {
+    queue.enqueue(i)
+  }
+
+  const dequeued = []
+  for (let i = 0; i < 10; i++) {
+    dequeued.push(queue.dequeue())
+  }
+
+  expect(queue.isEmpty()).toBeTruthy()
+  expect(dequeued.map(x => x.value)).toEqual(Array.from({length: 10}, (_, i) => i))
+})
+
+test('enqueue then tryDequeue', () => {
+  const queue = new LinkedListQueue<number>()
+  for (let i = 0; i < 10; i++) {
+    queue.enqueue(i)
+  }
+
+  const dequeued = []
+  for (let i = 0; i < 10; i++) {
+    dequeued.push(queue.tryDequeue(() => true))
+  }
+
+  expect(queue.isEmpty()).toBeTruthy()
+  expect(dequeued.map(x => x!.value)).toEqual(Array.from({length: 10}, (_, i) => i))
+})
+
+test('enqueue then tryDequeue reversed', () => {
+  const queue = new LinkedListQueue<number>()
+  for (let i = 0; i < 10; i++) {
+    queue.enqueue(i)
+  }
+
+  const dequeued = []
+  for (let i = 9; i >= 0; i--) {
+    dequeued.push(queue.tryDequeue(x => x === i))
+  }
+
+  expect(queue.isEmpty()).toBeTruthy()
+  expect(dequeued.map(x => x!.value)).toEqual(Array.from({length: 10}, (_, i) => 9 - i))
+})
+
+test('bug: should not corrupt queue state after tryDequeue the last element', () => {
   const queue = new LinkedListQueue<number>()
 
   queue.enqueue(1)
   queue.enqueue(2)
-  queue.tryDequeue(x => x === 2)
+  // Dequeue the last element
+  expect(queue.tryDequeue(x => x === 2)).toEqual({value: 2})
+
   queue.enqueue(3)
-  queue.tryDequeue(() => true)
-
-  expect(queue.isEmpty()).toBeFalsy()
-
-  queue.tryDequeue(() => true)
-
+  expect(queue.tryDequeue(() => true)).toEqual({value: 1})
+  expect(queue.tryDequeue(() => true)).toEqual({value: 3})
   expect(queue.isEmpty()).toBeTruthy()
 })
